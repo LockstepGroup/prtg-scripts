2e42d05098a3b69d10ecce8051bad9f019765eb0
Brian Addicks <brian@addicks.us>
2017-10-02 15:28:10 -0400

    start work on New-PrtgSensorScript

---
+++ tools/New-PrtgSensorScript.ps1
@@ -0,0 +1,62 @@
+#Requires
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$True,Position=0)]
+    [string]$ScriptName,
+
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$OutputDirectory,
+
+    [Parameter(Mandatory=$True,Position=2)]
+    [string]$AuthorEmail,
+
+    [Parameter(Mandatory=$True,Position=3)]
+    [string]$Description
+)
+
+# We'll use these to pull down the most recent version of Set-PrtgError and New-PrtgResult from github
+$PrtgShellRepo    = 'LockstepGroup/prtgshell2'
+$RawContentUrl    = 'https://raw.githubusercontent.com/' + $PrtgShellRepo
+$SetPrtgErrorUrl  = $RawContentUrl + '/master/src/cmdlets/Set-PrtgError.ps1'
+$NewPrtgResultUrl = $RawContentUrl + '/master/src/cmdlets/New-PrtgResult.ps1'
+
+$Header = @"
+###############################################################################
+#
+# ScriptName: $ScriptName
+# Auther: $AuthorEmail
+# Last Updated: Q4 2015
+# Monitors $Description
+#
+###############################################################################
+"@
+
+###############################################################################
+#                              PLACEHOLDER USAGE
+# prtg_sensorid ........................................................ UNUSED
+# prtg_deviceid ........................................................ UNUSED
+# prtg_groupid ......................................................... UNUSED
+# prtg_probeid ......................................................... UNUSED
+#
+# prtg_host ..................................................... $ComputerName
+# prtg_device .......................................................... UNUSED
+# prtg_group ........................................................... UNUSED
+# prtg_probe ........................................................... UNUSED
+# prtg_name ............................................................ UNUSED
+#
+# prtg_windowsdomain ................................................... UNUSED
+# prtg_windowsuser ..................................................... UNUSED
+# prtg_windowspassword ................................................. UNUSED
+#
+# prtg_linuxuser ....................................................... UNUSED
+# prtg_linuxpassword ................................................... UNUSED
+#
+# prtg_snmpcommunity ................................................... UNUSED
+#
+# prtg_version ......................................................... UNUSED
+# prtg_url ............................................................. UNUSED
+# prtg_primarychannel .................................................. UNUSED
+#
+###############################################################################
+
+
